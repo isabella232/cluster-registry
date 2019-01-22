586dfea196198c98207208982a61e74d44f3491b
Mikhail Mazurskiy <mmazurskiy@atlassian.com>
2019-01-22 14:04:44 +1100

    Make it compile with Kubernetes 1.13

--- pkg/client/clientset/versioned/typed/clusterregistry/v1alpha1/fake/fake_cluster.go
+++ pkg/client/clientset/versioned/typed/clusterregistry/v1alpha1/fake/fake_cluster.go
@@ -130,8 +130,9 @@ func (c *FakeClusters) DeleteCollection(options *v1.DeleteOptions, listOptions v
 
 // Patch applies the patch and returns the patched cluster.
 func (c *FakeClusters) Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.Cluster, err error) {
+	// EDITED MANUALLY TO MAKE IT COMPILE https://github.com/kubernetes/cluster-registry/issues/271
 	obj, err := c.Fake.
-		Invokes(testing.NewPatchSubresourceAction(clustersResource, c.ns, name, data, subresources...), &v1alpha1.Cluster{})
+		Invokes(testing.NewPatchSubresourceAction(clustersResource, c.ns, name, pt, data, subresources...), &v1alpha1.Cluster{})
 
 	if obj == nil {
 		return nil, err
